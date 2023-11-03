8adbf82c01bb27b4513aac1a6886a4787a4e3a6a
mklyuka <m_klyuka@inbox.ru>
2023-11-03 18:48:10 +0300

    add rso url

--- first_test/main.py
+++ first_test/main.py
@@ -2,25 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
  
-link = 'https://demo.applitools.com/'
+link = 'http://localhost:8080/'
 browser = webdriver.Chrome()
 browser.get(link)
  
-time.sleep(2)
- 
-username = browser.find_element(By.ID, 'username')
-username.send_keys('Maksim')
- 
-time.sleep(2)
- 
-password = browser.find_element(By.ID, 'password')
-password.send_keys('Qwerty1!')
- 
-time.sleep(2)
- 
-button = browser.find_element(By.ID, 'log-in')
-button.click()
- 
-time.sleep(2)
+time.sleep(7)
  
 browser.quit()

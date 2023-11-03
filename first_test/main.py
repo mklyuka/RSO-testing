07050245223e1cf88c3f45bd357ce59282cb61ee
mklyuka <m_klyuka@inbox.ru>
2023-11-03 18:39:19 +0300

    add example auto-test

--- first_test/main.py
+++ first_test/main.py
@@ -1,1 +1,26 @@
-print('Жопа')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+ 
+link = 'https://demo.applitools.com/'
+browser = webdriver.Chrome()
+browser.get(link)
+ 
+time.sleep(2)
+ 
+username = browser.find_element(By.ID, 'username')
+username.send_keys('Maksim')
+ 
+time.sleep(2)
+ 
+password = browser.find_element(By.ID, 'password')
+password.send_keys('Qwerty1!')
+ 
+time.sleep(2)
+ 
+button = browser.find_element(By.ID, 'log-in')
+button.click()
+ 
+time.sleep(2)
+ 
+browser.quit()

e6eefd41a495f7b0801a5df6cf8c8e1f50387bc4
mklyuka <m_klyuka@inbox.ru>
2023-11-07 11:00:27 +0300

    login action

--- first_test/main.py
+++ first_test/main.py
@@ -4,8 +4,17 @@ import time
  
 link = 'http://localhost:8080/'
 browser = webdriver.Chrome()
-browser.get(link)
  
-time.sleep(7)
- 
-browser.quit()
+try:
+    browser.get(link)
+
+    name = browser.find_element(By.CSS_SELECTOR, '[placeholder="Имя"]#name')
+    name.send_keys('ВинДизель')
+
+    password = browser.find_element(By.CSS_SELECTOR, '[placeholder="Пароль"]#password')
+    password.send_keys('Qwerty123')
+
+
+finally:
+    time.sleep(5)
+    browser.quit()
